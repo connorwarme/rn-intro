cdbab429c9b5775a3bcadae111d5e227aaa17327
Connor Warme <connor.warme@gmail.com>
2024-11-17 21:35:18 -0700

    feat: splash page, light status bar, etc

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,7 +1,10 @@
-import { View, StyleSheet } from "react-native";
+import { View, StyleSheet, Platform } from "react-native";
 import * as ImagePicker from "expo-image-picker";
-import { useState } from "react";
+import * as MediaLibrary from "expo-media-library";
+import { useState, useRef } from "react";
 import { GestureHandlerRootView } from "react-native-gesture-handler";
+import { captureRef } from "react-native-view-shot";
+import domtoimage from "dom-to-image";
 
 import ImageViewer from "@/components/ImageViewer";
 import Button from "@/components/Button";
@@ -20,7 +23,17 @@ export default function Index() {
   );
   const [showAppOptions, setShowAppOptions] = useState<boolean>(false);
   const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
-  const [selectedEmoji, setSelectedEmoji] = useState<ImageSource | undefined>(undefined)
+  const [selectedEmoji, setSelectedEmoji] = useState<ImageSource | undefined>(
+    undefined
+  );
+  // checks if there's permission to access the media library. if status is null, request permission (see below)
+  const [status, requestPermission] = MediaLibrary.usePermissions();
+  const imageRef = useRef<View>(null);
+
+  if (status === null) {
+    // use requestPermission method to request permission to access media library (to save image)
+    requestPermission();
+  }
   const pickImageAsync = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
       // pass in an object specifying the ImagePickerOptions:
@@ -50,16 +63,52 @@ export default function Index() {
   };
   const handleModalClose = () => {
     setIsModalVisible(false);
-  }
-  const handleSave = () => {};
+  };
+  const handleSave = async () => {
+    if (Platform.OS !== "web") {
+      try {
+        // captureRef method takes a ref and options object as arguments
+        // returns a promise that resolves to a URI string
+        const localUri = await captureRef(imageRef, {
+          height: 440,
+          quality: 1,
+        });
+        // saveToLibraryAsync method takes a URI string as an argument
+        // saves the image to the media library
+        await MediaLibrary.saveToLibraryAsync(localUri);
+        if (localUri) {
+          alert("Saved!");
+        }
+      } catch (e) {
+        console.log(e);
+      }
+    } else {
+      try {
+        const dataUrl = await domtoimage.toJpeg(imageRef.current, {
+          quality: 0.95,
+          width: 320,
+          height: 440,
+        });
+
+        let link = document.createElement("a");
+        link.download = "sticker-smash.jpeg";
+        link.href = dataUrl;
+        link.click();
+      } catch (e) {
+        console.log(e);
+      }
+    }
+  };
 
   return (
     <GestureHandlerRootView style={styles.container}>
       <View style={styles.imageContainer}>
-        <ImageViewer source={placeholderImage} selected={selectedImage} />
-        {selectedEmoji && (
-          <EmojiSticker imageSize={50} source={selectedEmoji} />
-        )}
+        <View ref={imageRef} collapsable={false}>
+          <ImageViewer source={placeholderImage} selected={selectedImage} />
+          {selectedEmoji && (
+            <EmojiSticker imageSize={50} source={selectedEmoji} />
+          )}
+        </View>
       </View>
       {showAppOptions ? (
         <View style={styles.optionsContainer}>
@@ -79,8 +128,11 @@ export default function Index() {
           <Button label="Use stock photo" onPress={handleStock} />
         </View>
       )}
-      <EmojiPicker isVisible={isModalVisible} onClose={handleModalClose} >
-        <EmojiList onSelect={setSelectedEmoji} onCloseModal={handleModalClose} />
+      <EmojiPicker isVisible={isModalVisible} onClose={handleModalClose}>
+        <EmojiList
+          onSelect={setSelectedEmoji}
+          onCloseModal={handleModalClose}
+        />
       </EmojiPicker>
     </GestureHandlerRootView>
   );
